4dccf42f00f1cbd8a2a2986b26945e486f70a096
voliynik <voleynik2016@gmail.com>
2017-05-15 12:52:41 +0300

    Add GNL

---
+++ libft/ft_strdup.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: voliynik <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/22 14:42:38 by voliynik          #+#    #+#             */
+/*   Updated: 2017/01/21 13:38:30 by voliynik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dup;
+
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!dup)
+		return (NULL);
+	ft_strcpy(dup, s1);
+	return (dup);
+}
